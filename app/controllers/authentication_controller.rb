c4fe53cb0ecb2200b42b8858085222f57635e7ed
wandji20 <wbertrand10@gmail.com>
2022-03-10 11:26:42 +0100

    Add authentication controller, update route to include login and add authentication request specs

---
+++ app/controllers/authentication_controller.rb
@@ -0,0 +1,4 @@
+class AuthenticationController < ApplicationController
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   post 'sign_up', to: 'users#create'
+  post 'login', to: 'authentication#create'
 end

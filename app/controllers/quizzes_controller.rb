6ba0f16228d3246e72694cf1863b572fe5584a53
wandji20 <wbertrand10@gmail.com>
2022-03-25 19:48:04 +0100

    Add header to apipie markup for quizzes index action

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -1,5 +1,6 @@
 class QuizzesController < ApplicationController
   api :GET, '/quizzes', 'Quizzes'
+  header :Authorization, 'Authentication token', required: true
   def index
     # call interaction/quizzes/index
     outcome = Quizzes::Index.run({ current_user: current_user })

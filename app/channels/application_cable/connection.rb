b6d73fb2a6ea2df0d370f98ab23b576780748756
wandji20 <wbertrand10@gmail.com>
2022-04-04 01:10:17 +0100

    Remove all Rest API controller, action cable, redis

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,10 +1,8 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
-    identified_by :current_user
+    # identified_by :current_user
 
-    # calls AuthorizeCableRequest to authenticate user JWT token
     def connect
-      self.current_user = AuthorizeCableRequest.call(request.params[:token])
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,7 @@
-require 'sidekiq/web'
-
 Rails.application.routes.draw do
   if Rails.env.development?
     mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "graphql#execute"
   end
   
   post "/graphql", to: "graphql#execute"
-
-  Sidekiq::Web.use ActionDispatch::Cookies
-  Sidekiq::Web.use Rails.application.config.session_store, Rails.application.config.session_options
-
-  mount Sidekiq::Web => "/sidekiq"
-
-  apipie
-  mount ActionCable.server => '/cable'
-  
-  root to: 'quizzes#index'
-  post 'sign_up', to: 'users#create'
-  post 'login', to: 'authentication#create'
-  get 'result', to: 'users#show'
-
-  resources :answered_questions, only: [:create, :update]
 end
